ec74a7c479f5b34c134802fd33e0a48272aaaf94
Raisess <raisechif@gmail.com>
2024-01-04 13:42:10 +0000

    chore: improve message struct

--- src/core/transporter.rs
+++ src/core/transporter.rs
@@ -1,10 +1,23 @@
+extern crate serde_json;
+
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Message {
   pub id: String,
   pub from: String,
   pub content: String,
+}
+
+impl Message {
+  pub fn from_str(message: &str) -> Result<Message, serde_json::Error> {
+    return serde_json::from_str(message);
+  }
+
+  pub fn to_string(message: &Message) -> String {
+    return serde_json::to_string(message).expect("Invalid JSON");
+  }
 }
 
 pub fn mount_message(from: &str, content: &str) -> Message {
